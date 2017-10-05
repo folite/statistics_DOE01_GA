e31d33c0d7a40d74f8ee070a089ae61404c6c376
folite <folite@example.com>
2017-10-05 17:58:32 +0800

    tset

--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 
 import random 
 
-maternal = []
+maternal = [] #母體
+maternalYield = [] #產出率
 
 def maternal_produce(): #母體建立 zfill長度不足補0
-    temp = bin(random.randint(100,350))[2:].zfill(9)
-    press = bin(random.randint(10,50))[2:].zfill(6)
-    time = bin(random.randint(10,30))[2:].zfill(5)
-    prm = bin(random.randint(2000,3000))[2:].zfill(12)
-    maternal.append(temp + press + time + prm)
+    temp = random.randint(100,350)
+    press = random.randint(10,50)
+    time = random.randint(10,30)
+    prm = random.randint(2000,3000)
+    print(temp)
+    print(press)
+    print(time)
+    print(prm)
+    #maternal.append(temp + press + time + prm)
 
-for x in range(10):
-     maternal_produce()
-print(maternal)
+def test(temp, press, time, prm):
+    f = 1 - abs(temp - 285) / 195 - abs(press * time - 60) / 90 - abs(time - 18) / 13 - 0.03
+    print(f)
+    a = f + 0.03 * prm - 1000 / 2000 
+    print(a)
+
+#for x in range(10):
+#maternal_produce()
+test(285, 3.3333, 18, 3000)
